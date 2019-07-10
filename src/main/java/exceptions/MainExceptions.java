b13dc2f654e74e4a1bf0f7c0af6d153621893c3f
VladimirMerzlov <Vladimir_merzlov@mail.ru>
2019-07-10 20:47:44 +0300

    HomeWork6(addException_Ver_1.1)

--- src/main/java/exceptions/MainExceptions.java
+++ src/main/java/exceptions/MainExceptions.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class MainExceptions {
     public static void main(String[] args) {
-       MyFailed myFailed = new MyFailed("Terminator");
-       MyFailed myFailed1 = new MyFailed("Chuck Norris");
         try {
-            int a = 5;
-            int b = 3;
-            System.out.println(5 / 3);
-            System.out.println(5 / 0);
+            MyFailed myFailed1 = new MyFailed("Chuck Norris");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите имя самого крутого из крутых ");
+            String temp = sc.nextLine();
+            MyFailed myFailed = new MyFailed(temp);
+
+//            int a = 5;
+//            int b = 3;
+//            System.out.println(5 / 3);
+//            System.out.println(5 / 0);
             myFailed.bestOfTheBest();
             myFailed1.bestOfTheBest();
             System.out.println(myFailed.getName());
@@ -19,9 +23,13 @@ public class MainExceptions {
             System.out.println(ex.getMessage());
 
         } catch (MyException myE) {
-            System.out.println("BestOfTheBestException");
+            System.out.println("BestOfTheBestException! - нет никого круче Чака Норисса");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите имя самого крутого из крутых ");
+            String temp = sc.nextLine();
+            MyFailed myFailed = new MyFailed(temp);
         } finally {
-            System.out.println("Это блок finally! Он выводится в любом случае");
+            System.out.println("Это был трудный выбор");
         }
     }
 }
